733effcc21e21d8d921e9345984b92be72938f69
willian <williandeitosi@gmail.com>
2025-02-06 20:47:13 -0300

    feat: add create user in service

--- apps/api/src/user/services/index.ts
+++ apps/api/src/user/services/index.ts
@@ -1,5 +1,28 @@
+import bcrypt from "bcrypt";
+import type { UserType } from "../../../../../packages/auth-schema";
+import { db } from "../../../prisma/db";
+
 export class UserService {
-  createUser() {
-    return "ola";
+  private async findByEmail(email: string) {
+    const user = await db.user.findUnique({ where: { email } });
+    return !!user;
+  }
+
+  async createUser({ email, password, name }: UserType) {
+    const isExists = await this.findByEmail(email);
+
+    if (isExists) {
+      throw new Error("User already exists!");
+    }
+
+    const hashPassword = await bcrypt.hash(password, 6);
+
+    const user = await db.user.create({
+      data: { email, password: hashPassword, name },
+    });
+
+    const { password: _, ...withoutPassword } = user;
+
+    return withoutPassword;
   }
 }
