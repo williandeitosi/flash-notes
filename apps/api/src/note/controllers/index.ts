f85f1733a490ef4accf360dbfda652fdc39895cb
willian <williandeitosi@gmail.com>
2025-02-07 15:36:13 -0300

    feat: add get node by id controller

--- apps/api/src/note/controllers/index.ts
+++ apps/api/src/note/controllers/index.ts
@@ -52,8 +52,43 @@ export class NoteController {
     try {
       const { id } = req.params;
       const userId = Number(id);
-      const allNote = await this.service.getllAllNotes(userId);
+      const allNote = await this.service.getAllNotes(userId);
       res.status(200).json({ allNote });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return res.status(400).json({
+          message: "validation error",
+          errors: error.errors.map((err) => ({
+            field: err.path.join("."),
+            message: err.message,
+          })),
+        });
+      }
+      if (error instanceof Error) {
+        return res.status(400).json({ error: error.message });
+      }
+
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+
+  async getNote(req: Request, res: Response) {
+    try {
+      const { id, noteId } = req.params;
+
+      const userId = Number(id);
+      if (!userId) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const note_id = Number(noteId);
+      if (!note_id) {
+        return res.status(404).json({ message: "Note not found" });
+      }
+
+      const getNote = await this.service.getNote(note_id, userId);
+
+      res.status(200).json({ getNote });
     } catch (error) {
       if (error instanceof ZodError) {
         return res.status(400).json({
