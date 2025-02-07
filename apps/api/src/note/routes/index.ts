4d6bdaa30ccd5e47fa9be85a6981094600e1500a
willian <williandeitosi@gmail.com>
2025-02-07 14:29:38 -0300

    feat: add get all notes

---
+++ apps/api/src/note/routes/index.ts
@@ -0,0 +1,17 @@
+import type { Application } from "express";
+import { NoteController } from "../controllers";
+import { NoteService } from "../services";
+
+const service = new NoteService();
+const controller = new NoteController(service);
+
+const noteRoutes = (app: Application) => {
+  app.post("/user/:id/new-note", (req, res) => {
+    controller.create(req, res);
+  });
+  app.get("/user/:id/notes", (req, res) => {
+    controller.getAll(req, res);
+  });
+};
+
+export default noteRoutes;
