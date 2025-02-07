c6368ac7234c857596f51b7d24c868b44b3127e7
willian <williandeitosi@gmail.com>
2025-02-07 17:28:33 -0300

    feat(controller): implement note update functionality

--- apps/api/src/note/controllers/index.ts
+++ apps/api/src/note/controllers/index.ts
@@ -1,15 +1,40 @@
 import type { Request, Response } from "express";
-import { ZodError } from "zod";
 import type { NoteService } from "../services";
+import { handleError, parseIds } from "../utils";
 import { noteSchema } from "./../../../../../packages/models/note-schema/index";
 
 export class NoteController {
   constructor(private service: NoteService) {}
 
+  // private handleError(error: unknown, res: Response) {
+  //   if (error instanceof ZodError) {
+  //     return res.status(400).json({
+  //       message: "Validation error",
+  //       errors: error.errors.map((err) => ({
+  //         field: err.path.join("."),
+  //         message: err.message,
+  //       })),
+  //     });
+  //   }
+  //   if (error instanceof Error) {
+  //     return res.status(400).json({ error: error.message });
+  //   }
+  //   return res.status(500).json({ error: "Internal Server Error" });
+  // }
+
+  // private parseIds(req: Request) {
+  //   const userId = Number(req.params.id);
+  //   const noteId = req.params.noteId ? Number(req.params.noteId) : null;
+
+  //   if (isNaN(userId)) throw new Error("Invalid User ID");
+  //   if (noteId !== null && isNaN(noteId)) throw new Error("Invalid Note ID");
+
+  //   return { userId, noteId };
+  // }
+
   async create(req: Request, res: Response) {
     try {
-      const { id } = req.params;
-      const userId = Number(id);
+      const { userId } = parseIds(req);
       const parseData = noteSchema.safeParse(req.body);
 
       if (!parseData.success) {
@@ -31,79 +56,59 @@ export class NoteController {
         ...newNote,
       });
     } catch (error) {
-      if (error instanceof ZodError) {
-        return res.status(400).json({
-          message: "validation error",
-          errors: error.errors.map((err) => ({
-            field: err.path.join("."),
-            message: err.message,
-          })),
-        });
-      }
-      if (error instanceof Error) {
-        return res.status(400).json({ error: error.message });
-      }
-
-      return res.status(500).json({ error: "Internal Server Error" });
+      handleError(error, res);
     }
   }
 
   async getAll(req: Request, res: Response) {
     try {
-      const { id } = req.params;
-      const userId = Number(id);
+      const { userId } = parseIds(req);
+
       const allNote = await this.service.getAllNotes(userId);
       res.status(200).json({ allNote });
     } catch (error) {
-      if (error instanceof ZodError) {
-        return res.status(400).json({
-          message: "validation error",
-          errors: error.errors.map((err) => ({
-            field: err.path.join("."),
-            message: err.message,
-          })),
-        });
-      }
-      if (error instanceof Error) {
-        return res.status(400).json({ error: error.message });
-      }
-
-      return res.status(500).json({ error: "Internal Server Error" });
+      handleError(error, res);
     }
   }
 
   async getNote(req: Request, res: Response) {
     try {
-      const { id, noteId } = req.params;
+      const { noteId, userId } = parseIds(req);
 
-      const userId = Number(id);
-      if (!userId) {
-        return res.status(404).json({ message: "User not found" });
-      }
+      if (!noteId)
+        return res.status(404).json({ message: "Note ID is required" });
 
-      const note_id = Number(noteId);
-      if (!note_id) {
-        return res.status(404).json({ message: "Note not found" });
-      }
-
-      const getNote = await this.service.getNote(note_id, userId);
+      const getNote = await this.service.getNote(noteId, userId);
 
       res.status(200).json({ getNote });
     } catch (error) {
-      if (error instanceof ZodError) {
+      handleError(error, res);
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    try {
+      const parseData = noteSchema.safeParse(req.body);
+
+      if (!parseData.success) {
         return res.status(400).json({
-          message: "validation error",
-          errors: error.errors.map((err) => ({
-            field: err.path.join("."),
-            message: err.message,
-          })),
+          message: "Invalid information",
+          erro: parseData.error.errors,
         });
       }
-      if (error instanceof Error) {
-        return res.status(400).json({ error: error.message });
-      }
+      const { description, title } = parseData.data;
+      const { userId, noteId } = parseIds(req);
+      if (!noteId) return res.status(404).json({ message: "Note not found" });
+      const note = await this.service.updateNote(
+        userId,
+        noteId,
+        description,
+        title
+      );
 
-      return res.status(500).json({ error: "Internal Server Error" });
+      res.status(200).json({ message: "Update note successfully!", ...note });
+    } catch (error) {
+      handleError(error, res);
     }
   }
 }
