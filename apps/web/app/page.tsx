dd726266c9ea2a065c7ff2070f80bd6cd0282ec5
willian <williandeitosi@gmail.com>
2025-02-22 09:55:19 -0300

    feat: create shared state

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 import ColumnNotes from "./components/notes/ColumnNotes";
 import ShowNote from "./components/notes/ShowNote";
 const queryClient = new QueryClient();
+
 export default function Home() {
+  const [noteId, setNoteId] = useState<number | null>(null);
+
   return (
     <QueryClientProvider client={queryClient}>
       <div className="h-screen w-full flex flex-col overflow-hidden p-2 bg-zinc-900 text-white">
@@ -12,10 +16,10 @@ export default function Home() {
 
         <div className="flex-1 overflow-hidden">
           <div className="grid grid-cols-12 h-full gap-4">
-            <ColumnNotes />
+            <ColumnNotes onSelectedNote={setNoteId} />
             <div className="col-span-9 border-sky-700 border-l-2 overflow-hidden rounded-lg">
               <h2 className="text-center text-sky-700 text-3xl">View</h2>
-              <ShowNote />
+              <ShowNote noteId={noteId} />
             </div>
           </div>
         </div>
