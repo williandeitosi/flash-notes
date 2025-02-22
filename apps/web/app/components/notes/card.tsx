46c777242bfb1d6d319fe24d485538624a12b24f
willian <williandeitosi@gmail.com>
2025-02-22 10:08:55 -0300

    fix: css

--- apps/web/app/components/notes/card.tsx
+++ apps/web/app/components/notes/card.tsx
@@ -1,18 +1,18 @@
 interface CardProps {
-  id: number;
   title: string;
   description: string;
   onClick: (id: number) => void;
+  id: number;
 }
 
-const Card = ({ title, description, onClick, id }: CardProps) => {
+const Card: React.FC<CardProps> = ({ id, title, description, onClick }) => {
   return (
     <div
-      onClick={onClick ? () => onClick(id) : undefined}
-      className=" bg-gray-300 w-[90%] mx-auto rounded-lg text-black p-2 flex flex-col gap-2 "
+      onClick={() => onClick(id)}
+      className=" border-2 text-white border-zinc-500 w-[90%] mx-auto rounded-lg p-2 flex flex-col gap-2 "
     >
-      <h3 className="font-semibold truncate">{title}</h3>
-      <p className="truncate">{description}</p>
+      <h3 className="font-semibold break-words truncate">{title}</h3>
+      <p className="break-words line-clamp-4">{description}</p>
     </div>
   );
 };
