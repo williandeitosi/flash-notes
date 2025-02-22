a08748d3d8e0348a46f1b70d8f9541196d8f3a24
willian <williandeitosi@gmail.com>
2025-02-22 12:26:51 -0300

    feat: add cursor pointer

--- apps/web/app/components/notes/card.tsx
+++ apps/web/app/components/notes/card.tsx
@@ -9,7 +9,7 @@ const Card: React.FC<CardProps> = ({ id, title, description, onClick }) => {
   return (
     <div
       onClick={() => onClick(id)}
-      className=" border-2 text-white border-zinc-500 w-[90%] mx-auto rounded-lg p-2 flex flex-col gap-2 "
+      className=" border-2 text-white border-zinc-500 w-[90%] mx-auto rounded-lg p-2 flex flex-col gap-2 cursor-pointer "
     >
       <h3 className="font-semibold break-words truncate">{title}</h3>
       <p className="break-words line-clamp-4">{description}</p>
