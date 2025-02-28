ac8b8b874368cc5723e19e72e847f512c951dfa7
williandeitosi <williandeitosi@gmail.com>
2025-02-28 19:49:42 -0300

    feat: implement break words

--- apps/web/app/components/card.tsx
+++ apps/web/app/components/card.tsx
@@ -11,8 +11,8 @@ const Card: React.FC<CardProps> = ({ id, title, description, onClick }) => {
       onClick={() => onClick(id)}
       className=" bg-gray-300 w-[90%] mx-auto rounded-lg text-black p-2 flex flex-col gap-2 "
     >
-      <h3 className="font-semibold truncate">{title}</h3>
-      <p className="truncate">{description}</p>
+      <h3 className="text-xl font-bold truncate">{title}</h3>
+      <p className="break-words line-clamp-4">{description}</p>
     </div>
   );
 };
