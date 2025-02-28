5c79df0f76c191b402512f190c97098b739d197a
williandeitosi <williandeitosi@gmail.com>
2025-02-28 19:50:01 -0300

    fix: path import

--- packages/models/note-schema/index.ts
+++ packages/models/note-schema/index.ts
@@ -1,8 +1,9 @@
 import { z } from "zod";
 
-export const noteSchema = z.object({
+const noteSchema = z.object({
   title: z.string().min(1, "Title is required").max(100, "Title is too long"),
   description: z.string().min(1, "Description is required"),
 });
+export default noteSchema;
 
 export type NoteType = z.infer<typeof noteSchema>;
