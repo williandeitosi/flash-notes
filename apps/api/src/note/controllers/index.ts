46a91f567f6f9878ea2f277c939e3ef8caee1bc3
williandeitosi <williandeitosi@gmail.com>
2025-02-28 19:49:02 -0300

    fix: remove comments

--- apps/api/src/note/controllers/index.ts
+++ apps/api/src/note/controllers/index.ts
@@ -1,36 +1,10 @@
 import type { Request, Response } from "express";
+import noteSchema from "../../../../../packages/models/note-schema";
 import type { NoteService } from "../services";
 import { handleError, parseIds } from "../utils";
-import { noteSchema } from "./../../../../../packages/models/note-schema/index";
 
 export class NoteController {
   constructor(private service: NoteService) {}
-
-  // private handleError(error: unknown, res: Response) {
-  //   if (error instanceof ZodError) {
-  //     return res.status(400).json({
-  //       message: "Validation error",
-  //       errors: error.errors.map((err) => ({
-  //         field: err.path.join("."),
-  //         message: err.message,
-  //       })),
-  //     });
-  //   }
-  //   if (error instanceof Error) {
-  //     return res.status(400).json({ error: error.message });
-  //   }
-  //   return res.status(500).json({ error: "Internal Server Error" });
-  // }
-
-  // private parseIds(req: Request) {
-  //   const userId = Number(req.params.id);
-  //   const noteId = req.params.noteId ? Number(req.params.noteId) : null;
-
-  //   if (isNaN(userId)) throw new Error("Invalid User ID");
-  //   if (noteId !== null && isNaN(noteId)) throw new Error("Invalid Note ID");
-
-  //   return { userId, noteId };
-  // }
 
   async create(req: Request, res: Response) {
     try {
